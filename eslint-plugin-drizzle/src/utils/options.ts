560015fcd5bc4a8844dc0bbe0606d8b87717d9a5
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-12 22:00:44 -0400

    fix(eslint): recognize private drizzle receivers

--- eslint-plugin-drizzle/src/utils/options.ts
+++ eslint-plugin-drizzle/src/utils/options.ts
@@ -20,6 +20,11 @@ const isDrizzleObjName = (name: string, drizzleObjectName: string[] | string) =>
 	return false;
 };
 
+const isNamedProperty = (
+	property: TSESTree.MemberExpression['property'],
+): property is TSESTree.Identifier | TSESTree.PrivateIdentifier =>
+	property.type === 'Identifier' || property.type === 'PrivateIdentifier';
+
 export const isDrizzleObj = (
 	node: TSESTree.MemberExpression,
 	options: Options,
@@ -28,12 +33,12 @@ export const isDrizzleObj = (
 
 	if (node.object.type === 'Identifier') {
 		return isDrizzleObjName(node.object.name, drizzleObjectName);
-	} else if (node.object.type === 'MemberExpression' && node.object.property.type === 'Identifier') {
+	} else if (node.object.type === 'MemberExpression' && isNamedProperty(node.object.property)) {
 		return isDrizzleObjName(node.object.property.name, drizzleObjectName);
 	} else if (node.object.type === 'CallExpression') {
 		if (node.object.callee.type === 'Identifier') {
 			return isDrizzleObjName(node.object.callee.name, drizzleObjectName);
-		} else if (node.object.callee.type === 'MemberExpression' && node.object.callee.property.type === 'Identifier') {
+		} else if (node.object.callee.type === 'MemberExpression' && isNamedProperty(node.object.callee.property)) {
 			return isDrizzleObjName(node.object.callee.property.name, drizzleObjectName);
 		}
 	}
